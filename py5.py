8c063dd7e2a548667e37a823d095ddba50f8c710
sohitfirs <126562924+sohitfirs@users.noreply.github.com>
2023-11-01 14:58:58 +0300

    Add files via upload

---
+++ py5.py
@@ -0,0 +1,25 @@
+﻿# Задание 1
+
+s1 = str(input())
+print(s1, s1[::-1])
+if s1 == s1[::-1]:
+    print('YES')
+else:
+    print('NO')
+
+# Задание 2
+
+s2 = str(input())
+if len(s2) > 1000:
+    print('ERROR')
+
+s2 = s2.strip()
+s2 = s2.split(' ')
+
+s3 = list('')
+
+for i in range(len(s2)):
+    if s2[i] != '':
+        str(s3.insert(i, s2[i]))
+
+print(' '.join(s3))
